31e63b3c14b4811111a50f582c22d62e7d2a994f
warmcotton <wonseok0807@naver.com>
2023-11-21 19:41:41 +0900

    Add Cart

---
+++ src/main/java/com/sws/danggeun/entity/Cart.java
@@ -0,0 +1,27 @@
+package com.sws.danggeun.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "cart")
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Cart {
+    @Id @Column(name = "cart_id") @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "user_id") @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @JoinColumn(name = "item_id") @ManyToOne(fetch = FetchType.LAZY)
+    private Item item;
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
