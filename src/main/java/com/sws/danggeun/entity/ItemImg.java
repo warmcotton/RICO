e7849b08222b87e6ab6e78b038b102b919ac87cd
warmcotton <wonseok0807@naver.com>
2023-11-21 23:17:56 +0900

    add ItemImg

---
+++ src/main/java/com/sws/danggeun/entity/ItemImg.java
@@ -0,0 +1,19 @@
+package com.sws.danggeun.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Entity
+@Table(name="item_img")
+@Getter @Setter
+public class ItemImg {
+    @Id @Column(name="item_img_id") @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imgName; //이미지 파일명
+    private String oriImgName; //원본 이미지 파일명
+    private String imgUrl; //이미지 조회 경로
+    private String repimgYn; //대표 이미지 여부
+    @ManyToOne(fetch = FetchType.LAZY) @JoinColumn(name = "item_id")
+    private Item item;
+}
