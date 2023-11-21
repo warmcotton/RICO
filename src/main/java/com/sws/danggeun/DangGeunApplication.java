a621de2b629a5829239f6e0527da58c2be7bb0df
warmcotton <wonseok0807@naver.com>
2023-11-21 17:37:22 +0900

    Initial commit

---
+++ src/main/java/com/sws/danggeun/DangGeunApplication.java
@@ -0,0 +1,13 @@
+package com.sws.danggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangGeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangGeunApplication.class, args);
+	}
+
+}
