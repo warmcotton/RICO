962f5fb7b9eab56a3d3dfc79666cf856184d69fe
wonseok0807 <wonseok0807@naver.com>
2023-11-24 03:17:57 +0900

    test: createCart

---
+++ src/test/java/com/sws/danggeun/service/CartServiceTest.java
@@ -0,0 +1,39 @@
+package com.sws.danggeun.service;
+
+import com.sws.danggeun.entity.Cart;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CartServiceTest {
+    @Autowired
+    CartService cartService;
+
+    Cart returnCart() {
+        Cart cart = cartService.createCart("sws@naver.com");
+        return cart;
+    }
+
+    @Test
+    void searchCartAfterCreateCart() {
+      Cart create = returnCart(); //1 transaction
+      Cart search = cartService.searchCart(create.getId()); //1 transaction
+
+      assertAll("create", () -> assertEquals(create.getId(),search.getId()));
+    }
+
+    @Test
+    void createCart() {
+        Cart cart = cartService.createCart("sws@naver.com");
+        assertEquals(1L,cart.getId());
+    }
+
+    @Test
+    void deleteCart() {
+    }
+}
