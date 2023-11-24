45380119cbbfdeb9a2057962d23ef1dbbac3be9c
wonseok0807 <wonseok0807@naver.com>
2023-11-24 19:24:08 +0900

    feat: Cart,CartItem Reduce

--- src/main/java/com/sws/danggeun/service/OrderService.java
+++ src/main/java/com/sws/danggeun/service/OrderService.java
@@ -2,10 +2,7 @@ package com.sws.danggeun.service;
 
 import com.sws.danggeun.dto.OrderDto;
 import com.sws.danggeun.entity.*;
-import com.sws.danggeun.repository.CartItemRepository;
-import com.sws.danggeun.repository.OrderItemRepository;
-import com.sws.danggeun.repository.OrderRepository;
-import com.sws.danggeun.repository.UserRepository;
+import com.sws.danggeun.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +12,15 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class OrderService {
+    private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
     private final CartItemRepository cartItemRepository;
     private final OrderItemRepository orderItemRepository;
     private final UserRepository userRepository;
-    public OrderDto order(List<Cart> cartList, String email) { //상품 수량 확인 -> ( 아이템 수량 차감 -> 주문 ) 하나의 트랜잭션
+    public Order order(List<Cart> cartList, String email) { //상품 수량 확인 -> ( 아이템 수량 차감 -> 주문 ) 하나의 트랜잭션
         User user = userRepository.findByEmail(email).get();
         Order newOrder = orderRepository.save(Order.getInstance(user, OrderStatus.ORDER));
-
         int total = 0;
-
         for (Cart cart : cartList) {
             List<CartItem> cartItem = cartItemRepository.findByCart(cart);
             int order_item_price = 0;
@@ -36,12 +32,16 @@ public class OrderService {
                     OrderItem newOrderItem = OrderItem.getInstance(citem.getItem(),newOrder,citem.getCount(), price);
                     orderItemRepository.save(newOrderItem);
                 }
+                cartItemRepository.delete(cit);
             }
             total += order_item_price;
+            cartRepository.delete(cart);
         }
-
         newOrder.setPrice(total);
-        OrderDto order = OrderDto.newInstance(newOrder);
-        return order;
+        return newOrder;
+    }
+    public List<Order> getOrders(String email) {
+        User user = userRepository.findByEmail(email).get();
+        return orderRepository.findAllByUser(user);
     }
 }
