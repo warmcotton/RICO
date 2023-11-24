7e6f79f3f6b480793a602c6dcaf02ac76dc3d61b
wonseok0807 <wonseok0807@naver.com>
2023-11-24 19:25:51 +0900

    add findAllByUser

--- src/main/java/com/sws/danggeun/repository/OrderRepository.java
+++ src/main/java/com/sws/danggeun/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.sws.danggeun.repository;
 
 import com.sws.danggeun.entity.Order;
+import com.sws.danggeun.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByUser(User user);
 }
