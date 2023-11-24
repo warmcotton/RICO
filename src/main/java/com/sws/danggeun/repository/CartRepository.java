e6bbce32f0d36c7ffe372e9c478e232671de2274
wonseok0807 <wonseok0807@naver.com>
2023-11-24 22:22:43 +0900

    fix: Method Name

--- src/main/java/com/sws/danggeun/repository/CartRepository.java
+++ src/main/java/com/sws/danggeun/repository/CartRepository.java
@@ -1,7 +1,11 @@
 package com.sws.danggeun.repository;
 
 import com.sws.danggeun.entity.Cart;
+import com.sws.danggeun.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findByUser(User user);
 }
--- src/main/java/com/sws/danggeun/repository/OrderRepository.java
+++ src/main/java/com/sws/danggeun/repository/OrderRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findAllByUser(User user);
+    List<Order> findByUser(User user);
 }
