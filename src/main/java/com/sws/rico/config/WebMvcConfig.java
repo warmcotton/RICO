ea16d129c3e73478b387fb8267728e43d3688d66
warmcotton <wonseok0807@naver.com>
2024-01-28 21:38:19 +0900

    cors

--- src/main/java/com/sws/rico/config/WebMvcConfig.java
+++ src/main/java/com/sws/rico/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.sws.rico.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("http://192.168.219.108:5500")
+                .allowedMethods("*");
     }
 }
